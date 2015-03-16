e43916116a8b96b693db84a6b3b4a82270a966f8
Wenson Tsai <wensontsai@gmail.com>
2015-03-16 14:44:36 -0400

    first

---
+++ routes/todos.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Todo = require('../models/Todo.js');
+
+/* GET all todos listing. */
+router.get('/', function(req, res, next) {
+  Todo.find(function (err, todos){
+    if (err) return next(err);
+    res.json(todos);
+  });
+});
+
+module.exports = router;
+
+
+// post and get todo
+router.post('/', function(req, res, next) {
+  Todo.create(req.body, function(err,post){
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  Todo.findById(req.params.id, function(err,post){
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  Todo.findByIdAndUpdate(req.params.id, req.body, function(err,post){
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  Todo.findByIdAndRemove(req.body, function(err,post){
+    if (err) return next(err);
+    res.json(post);
+  });
+});
